3963780051a41454e4e0294a5338a3aff01a60ba
victorucama-create <victorucama@gmail.com>
2025-12-01 15:12:19 +0200

    Update Customer.js

--- backend/src/models/Customer.js
+++ backend/src/models/Customer.js
@@ -1,1 +1,29 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
 
+const Customer = sequelize.define('Customer', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      len: [2, 255]
+    }
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+      notEmpty: true
+    }
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: true
