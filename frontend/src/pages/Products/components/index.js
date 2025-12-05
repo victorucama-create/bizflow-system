7595da33a37ebee8dad013e6c082f50b611e8dda
victorucama-create <victorucama@gmail.com>
2025-12-05 14:03:31 +0200

    Create index.js

---
+++ frontend/src/pages/Products/components/index.js
@@ -0,0 +1,4 @@
+export { default as ProductModal } from './ProductModal'
+export { default as DeleteConfirmModal } from './DeleteConfirmModal'
+export { default as BulkActions } from './BulkActions'
+export { default as Filters } from './Filters'
